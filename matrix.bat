f2dc648b2265d6eb76ef286500059ae5d51d60b0
L4w1i3t <84912726+L4w1i3t@users.noreply.github.com>
2025-02-25 08:22:34 -0500

    E

---
+++ matrix.bat
@@ -0,0 +1,9 @@
+@echo off
+pause
+title JOHN CENA DO DODO DOOOOOOOOOOOOOOO
+color 0b
+mode 1000
+
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
